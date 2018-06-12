ac79d6788ccdbeda9b474e9bd46e631f8eac3899
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-12 17:52:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+get '/name' do
+  "Hello Giuliana"
+end
+
+get '/hometown' do
+  "My homw town is Denver, Colorado (Aka, Weed-Town)"
+end
+
+get '/favorite-song' do 
+  "My favorite song is :
+  +Can I Get a Witness - SonReal,
+  +So Seductive - Kero One, Jeni Suk,
+  +She's Not There - The Zombies"
+end
+
 end
